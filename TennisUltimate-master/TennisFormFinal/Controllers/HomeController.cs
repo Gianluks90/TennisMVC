319ada0941277b345af587fdef7f1489ee860754
Gianluca Marino <45163497+Gianluks90@users.noreply.github.com>
2019-02-15 14:23:15 +0100

    Initial Commit

---
+++ TennisUltimate-master/TennisFormFinal/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TennisFormFinal.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TennisFormFinal.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        private ITSRepository repository;
+
+        public HomeController(ITSRepository repo)
+        {
+            repository = repo;
+        }
+        // GET: /<controller>/
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult ReservationForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult ReservationForm(TennisReservation reservation)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.AddReservation(reservation);
+                return View("BellaStoria");
+            }
+            else
+            {
+                // there is a validation error
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ViewResult DisplayList()
+        {
+           
+            return View(new TennisReservationViewModel { reservations = repository.Reservations });
+        }
+        [HttpPost]
+        public ViewResult DisplayList(TennisReservationViewModel model)
+        {
+                IEnumerable<TennisReservation> filtered = repository.Reservations;
+
+                if (!String.IsNullOrEmpty(model.PageInfo.FilterField))
+                {
+                    filtered = filtered.Where(r => r.Court.Name == model.PageInfo.FilterField);
+                }
+                if(model.PageInfo.FilterDate != null && model.PageInfo.FilterDate.Year!=1)
+                {
+                    filtered = filtered.Where(r => r.ReservationTime.ToShortDateString() == model.PageInfo.FilterDate.ToShortDateString());
+                }
+                return View(new TennisReservationViewModel { reservations = filtered });
+        }
+
+        //public ViewResult Delete(int id)
+        //{
+        //    repository.Delete(repository.Reservations.ToList()[id-1]);
+
+        //    return View("DisplayList",new TennisReservationViewModel() { reservations=repository.Reservations});
+        //}
+
+    }
+}
---
+++ TennisUltimate-master/TennisFormFinal/Models/TennisReservation.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace TennisFormFinal.Models
+{
+
+
+
+    public class TennisReservation
+    {
+        [Key]
+        public int ReservationId { get; set; }
+        public int CourtId { get; set; }
+        public Court Court { get; set; }
+
+        [DateRange]
+        public DateTime ReservationTime { get; set; }
+        public String MatchType { get; set; }
+    }
+
+    
+
+    public class DateRangeAttribute : RangeAttribute
+    {
+        public DateRangeAttribute()
+          : base(typeof(DateTime), DateTime.Now.ToShortDateString(), DateTime.Now.AddYears(2).ToShortDateString()) { }
+    }
+
+}
